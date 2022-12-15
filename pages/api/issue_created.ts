b26451aab14c6c956f3b2e81ab26327e3fb2bddf
Nicolas Marien <mariennicolas@icloud.com>
2022-12-15 16:44:31 +0100

    feat: add basic issue created sync

--- pages/api/issue_created.ts
+++ pages/api/issue_created.ts
@@ -1,10 +1,84 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Issue } from "@linear/sdk";
+import { Version3Client } from "jira.js";
 import type { NextApiRequest, NextApiResponse } from "next";
+const { JIRA_USER_EMAIL, JIRA_PAT, JIRA_PROJECT_KEY, JIRA_HOST } = process.env;
+import removeMarkdown from "markdown-to-text";
 import { LinearPayload } from "../../types";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { action, data, type, createdAt }: LinearPayload = req.body;
-  console.log(action, data, type, createdAt);
+const jiraClient = new Version3Client({
+  host: JIRA_HOST!,
+  authentication: {
+    basic: {
+      email: JIRA_USER_EMAIL!,
+      apiToken: JIRA_PAT!,
+    },
+  },
+});
 
-  res.status(200);
+function linearDescriptionToJiraDescription(description: string) {
+  const rawText = removeMarkdown(description);
+  return rawText
+    .split(/\n/)
+    .reduce<{ type: string; content: { text: string; type: string }[] }[]>(
+      (array, text) => {
+        if (text) {
+          array.push({
+            type: "paragraph",
+            content: [
+              {
+                text,
+                type: "text",
+              },
+            ],
+          });
+        }
+        return array;
+      },
+      []
+    );
+}
+
+async function createIssue(data: Issue) {
+  try {
+    await jiraClient.issues.createIssue({
+      fields: {
+        issuetype: {
+          name: "Story",
+        },
+        summary: data.title,
+        // NOTE: the content will have no formatting.
+        description: data.description
+          ? {
+              type: "doc",
+              version: 1,
+              content: linearDescriptionToJiraDescription(data.description),
+            }
+          : "",
+        project: {
+          key: JIRA_PROJECT_KEY!,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { action, data, type, createdAt }: LinearPayload = req.body;
+
+  switch (action) {
+    case "create":
+      await createIssue(data);
+      break;
+    default:
+      console.warn("Action not supported");
+      break;
+  }
+
+  res.status(200).send({});
 }
--- types.ts
+++ types.ts
@@ -1,10 +1,11 @@
 export type LinearObjectData = Record<string, string | boolean | number>;
+import { Issue } from "@linear/sdk";
 
 export type LinearPayload = {
   action: "create" | "update" | "remove";
   createdAt: string; // date format
   url: string;
   type: "Issue" | "Comment";
-  data: LinearObjectData;
-  updatedFrom: LinearObjectData;
+  data: Issue;
+  updatedFrom: Partial<Issue>;
 };
